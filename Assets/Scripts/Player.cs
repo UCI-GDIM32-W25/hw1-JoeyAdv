8bba68ee647eda83134bddd5a11872dec63a6dae
JoeyAdv <jsadvinc@uci.edu>
2025-01-10 11:25:54 -0800

    player movement
    completed player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,6 +18,44 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+
+        // PLAYER MOVEMENT
+        if (Input.GetKey(KeyCode.W) & Input.GetKey(KeyCode.A)){
+            _playerTransform.Translate(Vector3.up * _speed *Time.deltaTime);
+            _playerTransform.Translate(Vector3.left * _speed *Time.deltaTime);
+        } 
+        // Checks for southwest movement
+        else if (Input.GetKey(KeyCode.A) & Input.GetKey(KeyCode.S)){
+            _playerTransform.Translate(Vector3.left * _speed *Time.deltaTime);
+            _playerTransform.Translate(Vector3.down * _speed *Time.deltaTime);
+        } 
+        // Checks for southeast movement
+        else if (Input.GetKey(KeyCode.S) & Input.GetKey(KeyCode.D)){
+            _playerTransform.Translate(Vector3.down * _speed *Time.deltaTime);
+            _playerTransform.Translate(Vector3.right * _speed *Time.deltaTime);
+        } 
+        // Checks for northeast movement
+        else if (Input.GetKey(KeyCode.D) & Input.GetKey(KeyCode.W)){
+            _playerTransform.Translate(Vector3.right * _speed *Time.deltaTime);
+            _playerTransform.Translate(Vector3.up * _speed *Time.deltaTime);
+        } 
+        // checks for north movement
+        else if (Input.GetKey(KeyCode.W)){
+            _playerTransform.Translate(Vector3.up * _speed *Time.deltaTime);
+        } 
+        // checks for west movement
+        else if (Input.GetKey(KeyCode.A)){
+            _playerTransform.Translate(Vector3.left * _speed *Time.deltaTime);
+        } 
+        // checks for south movement
+        else if (Input.GetKey(KeyCode.S)){
+            _playerTransform.Translate(Vector3.down * _speed *Time.deltaTime);
+        } 
+        // checks for east movement
+        else if (Input.GetKey(KeyCode.D)){
+            _playerTransform.Translate(Vector3.right * _speed *Time.deltaTime);
+        }
+
         
     }
 
